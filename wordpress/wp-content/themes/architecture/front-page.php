20705d90486a890d4ba9493b000051f914454d88
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-03-30 22:13:56 +0200

    Added about section

--- wordpress/wp-content/themes/architecture/front-page.php
+++ wordpress/wp-content/themes/architecture/front-page.php
@@ -16,4 +16,25 @@
       </div>
     </div>
   </section>
+  <section class="about">
+    <div class="row">
+      <div class="about-wrapper">
+        <div class="left-part-about">
+          <div class="about-title">
+            <h1>20</h1>
+            <p>years of experience</p>
+          </div>
+        </div>
+        <div class="right-part-about">
+          <h2>About Us</h2>
+          <h1>
+            Break out of your routine with
+            a global perspective.
+          </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quod ratione consequatur explicabo corrupti quisquam tempore asperiores iusto, fuga rerum ducimus veritatis id eaque assumenda architecto enim, repellat ex alias.</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, quisquam ipsum! Eveniet eius error libero possimus, quos labore soluta et sunt fuga adipisci perspiciatis aliquid fugit. Asperiores vero ab repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo similique cumque maiores sed impedit facere, voluptates iste, consectetur enim magnam qui. Aut culpa dolorem in ipsum labore ea libero optio.</p>
+        </div>
+      </div>
+    </div>
+  </section>
 <?php get_footer(); ?>
