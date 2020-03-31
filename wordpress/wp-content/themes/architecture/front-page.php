e4a8a3565034d25932baaeed31d3b52174fa3f7a
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-03-31 10:56:27 +0200

    Added project two in projects section

--- wordpress/wp-content/themes/architecture/front-page.php
+++ wordpress/wp-content/themes/architecture/front-page.php
@@ -50,11 +50,13 @@
           </div>
           <div class="right-part-content">
             <h3>2010</h3>
-            <a href="" class="right-title">Poolscape Villa</a>
+            <a href="" class="right-title">
+              <h1>Poolscape Villa</h1>
+            </a>
             <p>
               The Seascape Villas project constitutes one of the first urban interventions in this very unique context, a landscape dominated by mountains and sea.
             </p>
-            <div class="project-btn">
+            <div class="project-btn-right">
               <a href="">read more
                 <span class="right-arrow"></span>
               </a>
@@ -62,8 +64,23 @@
           </div>
         </div>
         <div class="project">
-          <div class="left-part-content"></div>
-          <div class="right-part-img"></div>
+          <div class="left-part-content">
+            <h3>2010</h3>
+              <a href="" class="left-title">
+                <h1>Poolscape Villa</h1>
+              </a>
+              <p>
+                The Seascape Villas project constitutes one of the first urban interventions in this very unique context, a landscape dominated by mountains and sea.
+              </p>
+              <div class="project-btn-left">
+                <a href="">read more
+                  <span class="right-arrow"></span>
+                </a>
+              </div>
+            </div>
+          <div class="right-part-img">
+            <img src="<?php echo get_template_directory_uri();?>/assets/project2.jpg" alt="">
+          </div>
         </div>
       </div>
     </div>
