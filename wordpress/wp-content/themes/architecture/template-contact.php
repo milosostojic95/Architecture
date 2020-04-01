b8cca1260e6fc4e63c21261d4cae70c1b69c1e50
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-04-01 14:10:24 +0200

    Added content in contact form

--- wordpress/wp-content/themes/architecture/template-contact.php
+++ wordpress/wp-content/themes/architecture/template-contact.php
@@ -4,16 +4,29 @@
   */
   get_header();
 ?>
-<section class="contact">
+<section class="contact-hero">
   <div class="row">
-    <div class="contact-wrapper">
-      <div class="contact-title">
+    <div class="contact-hero-wrapper">
+      <div class="contact-hero-title">
         <h3><?php the_title();?></h3>
         <h1>Let's establish relations</h1>
       </div>
     </div>
   </div>
 </section>
+<section class="contact-form">
+  <div class="row">
+    <div class="contact-wrapper">
+      <div class="left-part-contact">
+        <h3><?php  the_title();?></h3>
+        <h1>Let's start new project.</h1>
+        <p>Now, as you were able to get a picture of who we are, it is up to you to contact us and lay the foundation for a new and successful business relationship. Our team consists</p>
+      </div>
+      <div class="right-part-contact">
 
+      </div>
+    </div>
+  </div>
+</section>
 <?php get_footer(); ?>
 
