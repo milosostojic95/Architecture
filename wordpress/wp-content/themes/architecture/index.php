6e299827923f38183c20cb853ed018a42d08664d
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-04-02 17:28:02 +0200

    Added blog items, add single page item

--- wordpress/wp-content/themes/architecture/index.php
+++ wordpress/wp-content/themes/architecture/index.php
@@ -1,36 +1,24 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Architecture
- */
-
-get_header();?>
-<section class="project-hero">
-    <div class="project-hero-wrapper">
-      <div class="project-hero-title">
-        <h3>Projects</h3>
-        <h1>This is what we doing</h1>
+<?php get_header(); ?>
+<section class="blog-hero">
+  <div class="blog-hero-wrapper">
+    <div class="blog-title">
+      <h3>WE STAND FOR</h3>
+      <h1>Having an idea. Being inspired.</h1>
+    </div>
+  </div>
+</section>
+<section class="blog-content">
+  <div class="row">
+  <h1>Projects</h1>
+    <div class="blog-content-wrapper">
+      <div class="blog-items">
+        <?php if(have_posts()) : ?>
+          <?php while(have_posts()) : the_post(); ?>
+            <?php get_template_part( 'content', get_post_format() ); ?>
+          <?php endwhile; ?>
+        <?php endif; ?>
       </div>
     </div>
-  </section>
-  <section class="blog">
-    <div class="row">
-    <?php 
-    if ( have_posts() ) {
-      while ( have_posts() ) {
-        the_post();
-        get_template_part( 'template-parts/content', get_post_type() );
-      } // end while
-    } // end if
-  ?>
-    </div>
-  </section>
-<?php get_footer();?>
+  </div>
+</section>
+<?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/architecture/templates/parts/loop/post-thumbnail.php
@@ -0,0 +1,12 @@
+<?php if(has_post_thumbnail()) : ?>
+	<div class="featured-image">
+    <?php if ( ! is_single() ) : ?>
+      <a href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail(); ?>
+      </a>
+    <?php else : ?>
+      <?php the_post_thumbnail(); ?>
+    <?php endif; ?>
+  </div>
+    <?php endif; ?>
+
