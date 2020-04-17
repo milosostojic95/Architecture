ab0fdbb61730c4932c192fd01427307f775bc70f
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-04-17 10:36:34 +0200

    Added Custom css sub menu

--- wordpress/wp-content/themes/architecture/inc/function-admin.php
+++ wordpress/wp-content/themes/architecture/inc/function-admin.php
@@ -4,12 +4,20 @@
   Admin page
 */
 function architecture_add_admin_page() {
+  // generate Architecture Admin Pgae
   add_menu_page('Architecture Theme Options', 'Architecture', 'manage_options', 'decor-architecture', 'architecture_theme_create_page', '
   dashicons-admin-customizer', 110);
+  // Generate Architecture Admin sub Page
+  add_submenu_page('decor-architecture', 'Architecture Theme Options', 'Settings', 'manage_options', 'decor-architecture', 'architecture_theme_create_page');
+  add_submenu_page('decor-architecture', 'Architecture Css Options', 'Custom Css', 'manage_options', 'decor-architecture-css', 'architecture_theme_settings_page');
 }
 add_action('admin_menu', 'architecture_add_admin_page');
 
 function architecture_theme_create_page() {
   // generation of our admin page
-  echo '<h1>Architecture Theme Options</h1>';
+
+}
+
+function architecture_theme_settings_page() {
+
 }
